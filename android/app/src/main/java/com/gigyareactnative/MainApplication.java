78e0215493012c3735824f4bbe03469054154078
ostogic <ostojicmilan395@gmail.com>
2017-08-31 15:57:26 +0100

    fixed xcode and android project files

--- android/app/src/main/java/com/gigyareactnative/MainApplication.java
+++ android/app/src/main/java/com/gigyareactnative/MainApplication.java
@@ -3,7 +3,6 @@ package com.gigyareactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -13,9 +12,8 @@ import com.gigya.socialize.android.GSAPI;
 import java.util.Arrays;
 import java.util.List;
 
-import com.facebook.FacebookSdk;
-
 public class MainApplication extends Application implements ReactApplication {
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -26,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
               new GigyaBridgePackage()
       );
     }
@@ -40,8 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-
-    GSAPI.getInstance().initialize(this, "3_L7xm_by92yHMggImKfw4wjX5znV51SSSZ8am6bj7HZGRYUilfMfshATkdBma_wxR", "au1.gigya.com");
+    GSAPI.getInstance().initialize(this, "3_n6HDyxNJWZ9C6j1JcQrexeRptHoiCayyzLQ0pXGd05WwXoYNaZAC83wQ2F7kCLWN", "eu1.gigya.com");
 
     SoLoader.init(this, /* native exopackage */ false);
   }
